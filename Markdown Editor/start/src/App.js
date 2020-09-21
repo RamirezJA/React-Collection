dc906e706325cffc9ec16e0ed5e2bb0c431f5c92
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-09-21 09:38:19 -0700

    Markdown editor :rocket:

--- Markdown Editor/start/src/App.js
+++ Markdown Editor/start/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMarkDown from 'react-markdown';
+
 import './App.css';
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('# sup')
+
+  function handleChange(e){
+    setMarkdown(e.target.value)
+  }
   return (
     <div className="app">
-      <textarea />
+      <textarea
+      onChange={handleChange}
+      value={markdown}/>
 
-      <div className="preview" />
+      <ReactMarkDown className='preview' source={markdown}/>
     </div>
   );
 }
